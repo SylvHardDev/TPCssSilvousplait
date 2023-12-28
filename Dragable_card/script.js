e27ee49dd0d5f3d959e6d4492ea59280f6b9102d
Hardy Sylvestre <hardsyldev@gmail.com>
2023-12-28 07:25:56 +0300

    misy bug ndrà

--- Dragable_card/script.js
+++ Dragable_card/script.js
@@ -1,10 +1,27 @@
 const carousel = document.querySelector(".carousel");
 const arrowBtns = document.querySelectorAll(".wrapper .fleche");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
 
 let isDragging = false,
   startScrollLeft,
   startX;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens
+  .slice(-cardPerView)
+  .reverese()
+  .forEach((card) => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+  });
+
+carouselChildrens
+  .slice(0, cardPerView)
+  .reverese()
+  .forEach((card) => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+  });
 
 arrowBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
