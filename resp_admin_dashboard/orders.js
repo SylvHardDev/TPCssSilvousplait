0fc4d16f5d31a97b229f3de7368ff0a58dbface8
Hardy Sylvestre <hardsyldev@gmail.com>
2023-12-29 13:53:22 +0300

    vita orders.js

---
+++ resp_admin_dashboard/orders.js
@@ -0,0 +1,20 @@
+const orders = [
+  {
+    productName: "JavaScript Tutorial",
+    productNumber: "87543",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    productName: "CSS full Course",
+    productNumber: "97525",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    productName: "Flex-Box Tutorial",
+    productNumber: "35535",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
