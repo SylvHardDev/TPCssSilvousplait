770185bc6b6fd3f447881b9ab1d262f036fd4f01
Hardy <hardsyldev@gmail.com>
2024-06-13 07:56:04 +0300

    dernier maj tsy de haiko lotra

--- faq-accordion-main/js/index.js
+++ faq-accordion-main/js/index.js
@@ -1,1 +1,10 @@
-const plus__1 = document.getElementById("")
+const p1 = document.getElementById("p1")
+const p2 = document.getElementById("p2")
+const p3 = document.getElementById("p3")
+const p4 = document.getElementById("p4")
+
+const m1 = document.getElementById("m1")
+const m2 = document.getElementById("m2")
+const m3 = document.getElementById("m3")
+const m4 = document.getElementById("m4")
+
