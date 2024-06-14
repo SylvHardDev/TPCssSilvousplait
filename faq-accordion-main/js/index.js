fe5b9bf4ad3206b41ccd50aa4fc55f63df8f21ee
Hardy <hardsyldev@gmail.com>
2024-06-14 20:35:53 +0300

    change classList au lieu de display

--- faq-accordion-main/js/index.js
+++ faq-accordion-main/js/index.js
@@ -1,24 +1,23 @@
-// Sélectionner tous les en-têtes d'accordéon
 const accordionHeaders = document.querySelectorAll('.accordion-header');
 
 accordionHeaders.forEach(header => {
     header.addEventListener('click', () => {
         const content = header.nextElementSibling;
 
-        // Vérifier si le contenu est actuellement caché
-        if (content.style.display === 'block') {
+        // Vérifier si le contenu est actuellement ouvert
+        if (content.classList.contains('open')) {
             // Masquer le contenu actuel
-            content.style.display = 'none';
+            content.classList.remove('open');
             header.querySelector('.icon').src = './assets/images/icon-plus.svg';
         } else {
             // Masquer tous les autres contenus
             document.querySelectorAll('.accordion-content').forEach(content => {
-                content.style.display = 'none';
+                content.classList.remove('open');
                 content.previousElementSibling.querySelector('.icon').src = './assets/images/icon-plus.svg';
             });
 
             // Afficher le contenu actuel
-            content.style.display = 'block';
+            content.classList.add('open');
             header.querySelector('.icon').src = './assets/images/icon-minus.svg';
         }
     });
